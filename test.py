4166578c89f4d8db6172f7cc9ed47bbd1df33e5f
gsuperuser <gaurangvinchurne.2306@gmail.com>
2025-11-18 13:19:08 +0530

    ex-02: caeser derypt function added

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
-def caesar(text, shift):
+def caesar(text, shift, encrypt = True):
     if not isinstance(shift, int):
         return 'Shift must be an integer'
     if shift > 25 or shift < 1:
         return 'Shift must be between 1 and 25'
-    
+
+    if not encrypt:
+        shift = -shift
+
+
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     shifted_text = alphabet[shift:] + alphabet[:shift]
     translation_table = str.maketrans(alphabet + alphabet.upper() , shifted_text+shifted_text.upper())
     return text.translate(translation_table)
 
-print(caesar('freeCodeCamp' ,1))
+encypted_text = 'Pbhentr vf sbhaq va hayvxryl cynprf.'
+
+def encrypt(text, shift):
+    return caesar(text, shift)
+
+def decrypt(text, shift):
+    return caesar(text, shift, False)
+
+print(decrypt(encypted_text, 13))
